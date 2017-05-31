80bc727e58277d0370f504aee1f711fdb1c6ddd1
Brandon Spencer <brandon.spencer@gmail.com>
2017-05-31 06:23:39 -0400

    updated formatting for results

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ var city_name = document.getElementById('city_name');
 var temp = document.getElementById('temp');
 var conditions = document.getElementById('conditions');
 var icon = document.getElementById('image');
+var fahr = document.getElementById('fahr');
+var celsius = document.getElementById('celsius');
+var kelvin = document.getElementById('kelvin');
 
 document.onreadystatchange = function() {
     if (document.readyState == "interactive") {
@@ -44,20 +47,14 @@ function httpRequestOnError() {
 function parseResults() {
 	
 	var results = JSON.parse(apiRequest.response);
-	var celsius = results.main.temp - 273.15
-	var fahr = celsius * 9/5 + 32;
 	var iconUrl = "http://openweathermap.org/img/w/" + results.weather[0].icon + ".png";
 	var img = 	document.createElement('img');
 	img.src = iconUrl;
-	// console.log(iconUrl);
-	city_name.innerHTML = results.name;
-	temp.innerHTML = 'Kelvin: ' + results.main.temp + 'K    Celsius: ' + celsius.toFixed(0) + '    Fahrenheit: ' + fahr.toFixed(0);
-	conditions.innerHTML = results.weather[0].description;
-	icon.innerHTML = $(".image").html(img);
-
-
+	fahr.innerHTML = Math.round(results.main.temp*9/5-459.67) + '&#176 F';
+	celsius.innerHTML = Math.round(results.main.temp - 273.15) + '&#176 C';
+	kelvin.innerHTML = Math.round(results.main.temp) + 'K';
+	city_name.innerHTML = '<p>' + results.name + '</p>';
+	conditions.innerHTML = '<p>' + results.weather[0].description + '</p>';
+	icon.innerHTML = '<p>' + $(".image").html(img) +'</p>';
 	$('#output').toggle();
-
-	console.log(results.coord.lon);
-	console.log('Parsing the api call');
 }
